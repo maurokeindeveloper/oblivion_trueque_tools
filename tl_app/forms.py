ffe8e107a599857789eeab5eecd88b32e24eea6b
ramiiaguirre <you@example.com>
2024-05-08 16:40:51 -0300

    mejoras de estilo en  Publicar Producto

--- tl_app/forms.py
+++ tl_app/forms.py
@@ -6,12 +6,24 @@ class CreacionDeProducto(forms.ModelForm):
         model = Producto
         fields = ['nombre', 'descripcion', 'categoria', 'imagen']
 
-    nombre = forms.CharField(label='Nombre del producto', max_length=50, widget=forms.TextInput(attrs={'class': 'input'})) 
+    nombre = forms.CharField(
+        label='Nombre del producto', 
+        max_length=50, 
+        widget=forms.TextInput(attrs={'class': 'input mb-2', 'style': 'width: 100%;'})
+    ) 
     descripcion = forms.CharField(
         label='Descripción',
         max_length=200,
         min_length=10,
-        widget=forms.Textarea(attrs={'rows': 3, 'cols': 40})
+        widget=forms.Textarea(attrs={'class': 'input mb-2', 'style': 'width: 100%;'})
     )
-    categoria = forms.ChoiceField(choices=Producto.Categoria.choices)
-    imagen = forms.ImageField(required=False)
+    categoria = forms.ChoiceField(
+        label='Categoria',
+        choices=Producto.Categoria.choices,
+        widget=forms.Select(attrs={'class': 'form-select mb-2', 'style': 'width: 100%;'})
+    )
+    imagen = forms.ImageField(
+        label="Imagen",
+        required=False,
+        widget=forms.FileInput(attrs={'class': 'btn btn-outline-dark mb-4', 'style': 'width: 100%;'})
+    )
