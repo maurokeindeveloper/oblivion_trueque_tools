81fb7b7eb170db01a19b26d351ecbb06473a9e42
mauro <maurokein1990@outlook.es>
2024-06-05 19:37:52 -0300

    implementando HU preguntar, responder, filtrar

--- tt_app/forms/producto_forms.py
+++ tt_app/forms/producto_forms.py
@@ -6,14 +6,14 @@ from django.core.exceptions import ValidationError
 import datetime
 from django.db import models
 
+
 class CreacionDeProducto(forms.ModelForm):
     nombre = forms.CharField(
         required=True,
         label="Nombre del producto *",
         max_length=50,
         widget=forms.TextInput(
-            attrs={ "class": "form-control",
-                    "placeholder":"Ingresá un nombre"}
+            attrs={"class": "form-control", "placeholder": "Ingresá un nombre"}
         ),
         error_messages={
             "required": "Debe ingresar nombre del producto",
@@ -25,8 +25,7 @@ class CreacionDeProducto(forms.ModelForm):
         max_length=1000,
         min_length=10,
         widget=forms.Textarea(
-            attrs={ "class": "form-control",
-                    "placeholder":"Describí tu producto"}
+            attrs={"class": "form-control", "placeholder": "Describí tu producto"}
         ),
         error_messages={
             "required": "Debe ingresar una descripción.",
@@ -38,54 +37,65 @@ class CreacionDeProducto(forms.ModelForm):
         label="Categoria *",
         choices=Producto.Categoria.choices,
         widget=forms.Select(
-            attrs={ "class": "form-select",
-                    "placeholder":"Seleccioná una categoría"}
+            attrs={"class": "form-select", "placeholder": "Seleccioná una categoría"}
         ),
         error_messages={
             "required": "Debe seleccionar una categoría.",
-        }
+        },
     )
     sucursal = forms.ModelChoiceField(
         required=True,
         label="Sucursal de preferencia *",
-        queryset=Sucursal.objects.exclude(activo=False).order_by("ciudad"), #el query del modelo a mostrar como seleccionables (se imprime su def __str__(self) definido en models.py )
-        empty_label=None, #esto es porque el forms.ModelChoiceField por defecto deja elegir vacío.
-        widget=forms.Select(
-            attrs={ "class": "form-select",
-                    "placeholder":""}
-        ),
+        queryset=Sucursal.objects.exclude(activo=False).order_by(
+            "ciudad"
+        ),  # el query del modelo a mostrar como seleccionables (se imprime su def __str__(self) definido en models.py )
+        empty_label=None,  # esto es porque el forms.ModelChoiceField por defecto deja elegir vacío.
+        widget=forms.Select(attrs={"class": "form-select", "placeholder": ""}),
         error_messages={
             "required": "Debe seleccionar una sucursal.",
-        }
+        },
     )
     imagen = forms.ImageField(
         required=False,
         label="Imagen",
-        widget=forms.FileInput(
-            attrs={"class": "btn btn-outline-dark"}
-        ),
+        widget=forms.FileInput(attrs={"class": "btn btn-outline-dark"}),
     )
     aceptar_terminos = forms.BooleanField(
         required=True,
         label="Aceptar términos y condiciones *",
-        widget=forms.CheckboxInput(
-            attrs={"class": "form-check-input mb-3"}
-        ),
+        widget=forms.CheckboxInput(attrs={"class": "form-check-input mb-3"}),
         error_messages={
             "required": "Debe aceptar los términos y condiciones para continuar.",
         },
     )
+
     class Meta:
         model = Producto
         fields = ["nombre", "descripcion", "categoria", "sucursal", "imagen"]
+
     def __init__(self, *args, **kwargs):
-        super(CreacionDeProducto,self).__init__(*args, **kwargs)              #Aca van las clases bootstrap a aplicar en todos los widgets de este form
-        fields = self.fields.copy()     #Esto es para no afectar el dict que usa la pagina (sino se borra el ultimo item)
-        fields.pop("aceptar_terminos")  #Aca sacamos el ultimo item(aceptar terminos) para no aplicarle los cambios
+        super(CreacionDeProducto, self).__init__(
+            *args, **kwargs
+        )  # Aca van las clases bootstrap a aplicar en todos los widgets de este form
+        fields = (
+            self.fields.copy()
+        )  # Esto es para no afectar el dict que usa la pagina (sino se borra el ultimo item)
+        fields.pop(
+            "aceptar_terminos"
+        )  # Aca sacamos el ultimo item(aceptar terminos) para no aplicarle los cambios
         for field in fields:
-            self.fields[field].widget.attrs["class"] += " " + "w-100 mt-2 mb-3" #IMPORTANTE: += para que no sobreesrciba las clases individuales de cada uno, Y MUY IMPORTANTE el primer espacio en " " porque sino se aplica mal.(se concatena con la ultima letra de la ultima clase individualmente definida)
+            self.fields[field].widget.attrs["class"] += (
+                " " + "w-100 mt-2 mb-3"
+            )  # IMPORTANTE: += para que no sobreesrciba las clases individuales de cada uno, Y MUY IMPORTANTE el primer espacio en " " porque sino se aplica mal.(se concatena con la ultima letra de la ultima clase individualmente definida)
+
 
 class FormularioDePregunta(forms.ModelForm):
     class Meta:
         model = Pregunta
+        fields = ["texto"]
+
+
+class FormularioDeRespuesta(forms.ModelForm):
+    class Meta:
+        model = Respuesta
         fields = ["texto"]
--- tt_app/views/producto_view.py
+++ tt_app/views/producto_view.py
@@ -1,26 +1,34 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from ..forms.producto_forms import CreacionDeProducto, FormularioDePregunta
+from ..forms.producto_forms import (
+    CreacionDeProducto,
+    FormularioDePregunta,
+    FormularioDeRespuesta,
+)
 from ..forms.usuario_forms import check_cliente
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
-from ..models import Producto
+from ..models import Producto, Pregunta, Respuesta
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
 
 @login_required
 def crear_producto(request):
-    chk=check_cliente(request.user)
+    chk = check_cliente(request.user)
     if chk["ok"]:
         return chk["return"]
     if request.POST:
         form = CreacionDeProducto(request.POST, request.FILES)
         if form.is_valid():
-            producto = form.save(commit=False)  
-            producto.usuario = request.user # Asignar el usuario autenticado al producto
-            producto.fecha_de_publicacion = timezone.now()  # Establecer la fecha de publicación antes de guardar
-            producto.save() # guardamos el producto
+            producto = form.save(commit=False)
+            producto.usuario = (
+                request.user
+            )  # Asignar el usuario autenticado al producto
+            producto.fecha_de_publicacion = (
+                timezone.now()
+            )  # Establecer la fecha de publicación antes de guardar
+            producto.save()  # guardamos el producto
             # Redirigir a la página de productos con mensaje de feedback
             return redirect(
                 reverse("productos")
@@ -28,12 +36,16 @@ def crear_producto(request):
             )
     else:
         form = CreacionDeProducto()
-    return render(request, "productos/crear_producto.html", {    # enviamos los siguientes parámetros:
-        "form": form,   # el form definido en producto_forms.py
-        "titulo": "Publicar producto", # el titulo del form
-        "boton": "Aceptar", # el texto del botón de confirmación
-        "obligatorios": True, # mostrar la advertencia de campos obligatorios o no
-    })
+    return render(
+        request,
+        "productos/crear_producto.html",
+        {  # enviamos los siguientes parámetros:
+            "form": form,  # el form definido en producto_forms.py
+            "titulo": "Publicar producto",  # el titulo del form
+            "boton": "Aceptar",  # el texto del botón de confirmación
+            "obligatorios": True,  # mostrar la advertencia de campos obligatorios o no
+        },
+    )
 
 
 def productos(request):
@@ -51,10 +63,14 @@ def buscar_productos(request):
             descripcion__icontains=cadena
         )
         productos = productos_nom.union(productos_desc).order_by("-promocionado")
-        return render(request,"productos/buscar_productos.html",{
-            "productos": productos,
-            "cadena": cadena,
-        })
+        return render(
+            request,
+            "productos/buscar_productos.html",
+            {
+                "productos": productos,
+                "cadena": cadena,
+            },
+        )
     else:
         return render(request, "productos/productos.html", {"productos": productos})
 
@@ -63,7 +79,7 @@ def detalle_producto(request, id):
     producto = get_object_or_404(Producto, id=id)
 
     # Lista de preguntas acerca del producto
-    preguntas = producto.preguntas.order_by("-fecha")
+    preguntas = producto.preguntas.all().order_by("-fecha")
 
     # Formulario para las preguntas de los clientes
     form = FormularioDePregunta()
@@ -74,9 +90,10 @@ def detalle_producto(request, id):
         {"producto": producto, "preguntas": preguntas, "formulario": form},
     )
 
+
 @login_required
 def preguntar(request, id):
-    chk=check_cliente(request.user)
+    chk = check_cliente(request.user)
     if chk["ok"]:
         return chk["return"]
     producto = get_object_or_404(Producto, id=id)
@@ -87,9 +104,9 @@ def preguntar(request, id):
     if form.is_valid():
         # Crea un objeto Pregunta sin guardarlo en la base de datos
         pregunta = form.save(commit=False)
-        # Asigna el producto y el client a la pregunta - DEBEN SER LOS OBJETOS, NO LOS VALORES (O IDS)
+        # Asigna el producto y el cliente a la pregunta - DEBEN SER LOS OBJETOS, NO LOS VALORES (O IDS)
         pregunta.producto = producto
-        pregunta.cliente = producto.usuario
+        pregunta.cliente = request.user
         # Guarda la pregunta en la base de datos
         pregunta.save()
     return render(
@@ -97,3 +114,48 @@ def preguntar(request, id):
         "productos/pregunta.html",
         {"producto": producto, "form": form, "pregunta": pregunta},
     )
+
+
+@login_required
+def responder(request, id):
+    chk = check_cliente(request.user)
+    if chk["ok"]:
+        return chk["return"]
+    pregunta = get_object_or_404(Pregunta, id=id)
+    respuesta = None
+    # Booleano para controlar la vista
+    enviada = False
+    # Se responde la pregunta
+    form_respuesta = FormularioDeRespuesta(data=request.POST)
+    if form_respuesta.is_valid():
+        # Crea un objeto Respuesta sin guardarlo en la base de datos
+        respuesta = form_respuesta.save(commit=False)
+        respuesta.pregunta = pregunta
+        # Guarda la respuesta en la base de datos
+        respuesta.save()
+        enviada = True
+        # Asigna la respuesta a la pregunta correspondiente y actualiza la BD
+        pregunta.respuesta = respuesta
+        pregunta.save()
+    return render(
+        request,
+        "productos/respuesta.html",
+        {"form": form_respuesta, "pregunta": pregunta, "enviada": enviada},
+    )
+
+
+def filtrar_productos(request, categoria):
+    nombre_categoria = {
+        1: "$0-$5000",
+        2: "$5000-$10000",
+        3: "$10000+",
+    }
+    if request.method == "GET":
+        productos = Producto.objects.exclude(activo=False).filter(categoria=categoria)
+        return render(
+            request,
+            "productos/filtrar_productos.html",
+            {"productos": productos, "categoria": nombre_categoria.get(categoria)},
+        )
+    else:
+        return render(request, "productos/productos.html", {"productos": productos})
