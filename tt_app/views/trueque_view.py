f8fbf3b6ab8ec893b95cd3530a166bd126bf18d3
unknown <felix55oyuela@gmail.com>
2024-06-05 18:01:55 -0300

    listados de trueques del cliente

--- tt_app/views/trueque_view.py
+++ tt_app/views/trueque_view.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from ..forms.usuario_forms import check_cliente,check_empleado
 from ..models import Trueque
@@ -12,21 +13,38 @@ def gestion_trueque(request):
 
 @login_required
 def trueques_entrantes(request):
-    chk=check_cliente(request.user)
+    usuario = request.user
+    chk=check_cliente(usuario)
     if chk["ok"]:
         return chk["return"]
-    usuario = request.user
     print(f"Usuario autenticado: {usuario.email}")
     #trueques = Trueque.objects.all()
     trueques = Trueque.objects.exclude(activo=False).filter(producto_solicitado__usuario=usuario , estado=1)
-
     return render(request, "trueques/trueques_entrantes.html", {"trueques": trueques})
 
 @login_required
 def trueques_salientes(request):
-    chk=check_cliente(request.user)
+    usuario = request.user
+    chk=check_cliente(usuario)
     if chk["ok"]:
         return chk["return"]
+    trueques = Trueque.objects.exclude(activo=False).filter(Q(estado=1) | Q(estado=2) , producto_solicitante__usuario=usuario).order_by("producto_solicitante")
+    return render(request, "trueques/trueques_salientes.html", {"trueques": trueques})
+
+@login_required
+def trueques_por_concretar(request):
     usuario = request.user
-    trueques = Trueque.objects.exclude(activo=False).filter(producto_solicitante__usuario=usuario , estado=1)
-    return render(request, "trueques/trueques_salientes.html", {"trueques": trueques, "nombre": usuario.first_name})
+    chk=check_cliente(usuario)
+    if chk["ok"]:
+        return chk["return"]
+    trueques = Trueque.objects.exclude(activo=False).filter(Q(producto_solicitante__usuario=usuario) | Q(producto_solicitado__usuario=usuario), estado=3).order_by("-fecha")
+    return render(request, "trueques/trueques_por_concretar.html", {"trueques": trueques})
+
+@login_required
+def trueques_finalizados(request):
+    usuario = request.user
+    chk=check_cliente(usuario)
+    if chk["ok"]:
+        return chk["return"]
+    trueques = Trueque.objects.exclude(activo=False).filter(Q(producto_solicitante__usuario=usuario) | Q(producto_solicitado__usuario=usuario), estado__gte=4).order_by("-fecha")
+    return render(request, "trueques/trueques_finalizados.html", {"trueques": trueques})
